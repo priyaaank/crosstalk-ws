56fa142f9cefee1619c9339de5f15ee19a38d1ac
Priyank <priyaaank@gmail.com>
2011-03-13 23:04:25 +0530

    Generated a rails 3 app skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CrosstalkWs::Application.config.secret_token = 'f9345ff0c40547b048b7c166dffc66c5e8cf5c7d4009e9e84a68c3db31df65c6215806013b5353ab6122aabe1204bae4f5955e63f334b192a548fb0ba145c730'
